e211fc96701f6a48e30356832955411eabb1ba0b
Zach Allen <FractalMachinist@gmail.com>
2022-01-05 16:00:23 -0500

    Changed historical results to render as Editors. TODO: This is awful, but pretty.

--- marknotes-client/src/MDComponents/SingleR.js
+++ marknotes-client/src/MDComponents/SingleR.js
@@ -1,11 +1,16 @@
 import "./SingleR.css"
 import styles from "./SingleR.module.css"
 import HeaderStack from "./HeaderStack"
+import Editor from "../Editor"
+import ReactMarkdown from "react-markdown"
 
-export default function SingleR({headers, date, MDText, children, snipHeaderDepth=0}){
+
+export default function SingleR({headers, date, MDText, snipHeaderDepth=0}){
     return <div className={styles.SingleRRoot}>
             <HeaderStack headers={headers.slice(snipHeaderDepth, headers.length - 1)}/>
             <i>{date}</i>
-            <p style={{"whiteSpace": "pre-wrap"}}>{MDText}</p>
+            {/* <ReactMarkdown children={MDText}/> */}
+            <Editor value={MDText} setValue={()=>{}} readOnly={"nocursor"}/>
+            {/* <p style={{"whiteSpace": "pre-wrap"}}>{MDText}</p> */}
         </div>
 }
